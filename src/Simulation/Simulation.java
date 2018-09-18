7627670674436c95f6194658e7e2290188b5b3d3
Amy <yk154@duke.edu>
2018-09-18 18:12:02 -0400

    simulation abstract

---
+++ src/Simulation/Simulation.java
@@ -0,0 +1,46 @@
+/**
+ *
+ */
+public abstract class Simulation {
+    public int getEmptyCells;
+    public int getFreqs;
+    /**
+     *
+     * @param row
+     * @param col
+     */
+    public void reset(int row, int col){
+
+    }
+
+
+    /**
+     *
+     * @param row
+     * @param col
+     */
+    public void start(int row, int col){
+
+    }
+
+
+    /**
+     *
+     * @param row
+     * @param col
+     */
+    public void stop(int row, int col){
+
+    }
+
+
+    /**
+     *
+     * @param row
+     * @param col
+     */
+    public void initializeGrid(int row, int col){
+
+    }
+
+}
