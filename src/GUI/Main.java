9572427dba571f3481564140db0bab80cbfc2f3b
Amy <yk154@duke.edu>
2018-09-18 18:30:47 -0400

    main class

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -1,4 +1,29 @@
 package GUI;
 
-public class Main{
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) {
+//        stage.setScene(myScene);
+        stage.setTitle("Welcomes To BreakOut!");
+        stage.show();
+
+//        var frame = new KeyFrame(Duration.millis(milli_delay), e -> gameMode(second_delay, stage));
+//        animation.setCycleCount(Timeline.INDEFINITE);
+//        animation.getKeyFrames().add(frame);
+//        animation.play();
+
+    }
+
+    //Start scene
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+module cellsociety.team16 {
+    requires javafx.base;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.media;
+
+    exports Cell;
+    exports Grid;
+    exports GUI;
+    exports Simulation;
+}
