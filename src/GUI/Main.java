f8604462ecf240307b7ae073ff728d6b528bebcc
Brooke Keene <bzk2@duke.edu>
2018-09-18 19:42:17 -0400

    new scene setup

--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -3,25 +3,54 @@ package GUI;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
+
+/**
+ * Main
+ *
+ * @author Brooke Keene
+ */
 public class Main extends Application {
+    public static final String TITLE = "Cell Society";
+    public static final int WIDTH = 850;
+    public static final int HEIGHT = 600;
+    public static final Paint BACKGROUND = Color.WHITE;
+    public static final int FRAMES_PER_SECOND = 60;
+    public static final int MILLISECOND_DELAY = 1000 / FRAMES_PER_SECOND;
+    public static final double SECOND_DELAY = 1.0 / FRAMES_PER_SECOND;
+
 
     @Override
     public void start(Stage stage) {
-//        stage.setScene(myScene);
-        stage.setTitle("Welcomes To BreakOut!");
+        UISetup myDisplay = new UISetup(WIDTH, HEIGHT, BACKGROUND);
+        stage.setScene(myDisplay.getScene());
+        stage.setTitle(TITLE);
         stage.show();
 
-//        var frame = new KeyFrame(Duration.millis(milli_delay), e -> gameMode(second_delay, stage));
-//        animation.setCycleCount(Timeline.INDEFINITE);
-//        animation.getKeyFrames().add(frame);
-//        animation.play();
+        var frame = new KeyFrame(Duration.millis(MILLISECOND_DELAY), e -> step(SECOND_DELAY));
+        var animation = new Timeline();
+        animation.setCycleCount(Timeline.INDEFINITE);
+        animation.getKeyFrames().add(frame);
+        animation.play();
 
     }
 
-    //Start scene
+    /**
+     * Animation Loop
+     *
+     * @param elapsedTime
+     */
+    private void step(double elapsedTime) {
+
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
--- src/GUI/UISetup.java
+++ src/GUI/UISetup.java
@@ -1,11 +1,39 @@
 package GUI;
 
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.paint.Paint;
+
+
+/**
+ * UISetup
+ *
+ * creates a scene for UI, adds other UI elements,
+ * and includes methods to return a scene
+ *
+ * @author Brooke Keene
+ */
 public class UISetup {
-    
+    private Scene myScene;
+    private Group myRoot;
+
     /**
      * Constructor
+     *
+     * @param width
+     * @param height
+     * @param color
      */
-    public UISetup() {
+    public UISetup(int width, int height, Paint color) {
+        myRoot = new Group();
+        myScene = new Scene(myRoot, width, height, color);
+    }
 
+    /**
+     *
+     * @return myScene
+     */
+    public Scene getScene() {
+        return myScene;
     }
 }
