68be412deb0539d3f5206997b8caa1b33e8f895a
Orgil Batzaya <ob29@duke.edu>
2018-09-21 21:14:26 -0400

    getting new grid

--- src/Grid/Grid.java
+++ src/Grid/Grid.java
@@ -6,38 +6,18 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Grid extends AbstractGrid {
-    int size;
-    protected List<List<Cell>> myCells;
+    //int size;
+    //protected List<List<Cell>> myCells;
 
     public Grid(int size, List<List<Cell>> cells){
         super(size, cells);
-        this.size = size;
-        myCells = cells;
+        //this.size = size;
+        //myCells = cells;
 
 
     }
 
-    public Cell getCellAt(int x, int y) {
-        return myCells.get(x).get(y);
-    }
 
-    public List<Cell> getCellsNear(Cell cell) {
-        int xPos = cell.getX();
-        int yPos = cell.getY();
-        List<Cell> cellsNear = new ArrayList<Cell>();
-        //adjacent
-        cellsNear.add(myCells.get(xPos - 1).get(yPos));
-        cellsNear.add(myCells.get(xPos + 1).get(yPos));
-        cellsNear.add(myCells.get(xPos).get(yPos - 1));
-        cellsNear.add(myCells.get(xPos).get(yPos + 1));
-        //diagonals
-        cellsNear.add(myCells.get(xPos - 1).get(yPos - 1));
-        cellsNear.add(myCells.get(xPos - 1).get(yPos + 1));
-        cellsNear.add(myCells.get(xPos + 1).get(yPos - 1));
-        cellsNear.add(myCells.get(xPos + 1).get(yPos + 1));
-
-        return cellsNear;
-    }
 
     public List<Cell> getEmptyCells(int emptyVal){
         List<Cell> requiredCells = new ArrayList<>();
