50b22d1c2d5c88ad857bbc828c7df5917d781199
Brooke Keene <bzk2@duke.edu>
2018-09-24 01:21:49 -0400

    flexible xml input

--- src/GUI/UISetup.java
+++ src/GUI/UISetup.java
@@ -24,7 +24,7 @@ public class UISetup {
     public static final String RESOURCE_PACKAGE = "resources/";
     public static final String UI_TEXT = "English";
     public static final String STYLESHEET = "default.css";
-    public static final String DEFAULT_TYPE = "life";
+    public static final String DEFAULT_TYPE = "Life";
     public static final int DEFAULT_GRID = 4;
     public static final double DEFAULT_INTERVAL = 1.0;
 
@@ -41,7 +41,6 @@ public class UISetup {
 
     private String simType;
     private int gridSize;
-//    private double interval = 0.5; // Simulation speed
     private int borderSpace = 10;
 
     /**
@@ -74,7 +73,7 @@ public class UISetup {
         myControls = new simControls(mySimulation, myStage, myBorder, myResources);
         myControls.addButtons();
         myControls.makeSideMenu();
-
+        System.out.println(simType);
         myGrid = new simGrid(gridSize, simType, myBorder);
     }
 
