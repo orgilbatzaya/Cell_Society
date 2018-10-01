aa9dd8184b379f526bcd486532f124b199747922
Brooke Keene <bzk2@duke.edu>
2018-09-30 20:44:56 -0400

    new class for grid flexibility

--- src/GUI/gridControls.java
+++ src/GUI/gridControls.java
@@ -1,4 +1,14 @@
 package GUI;
 
+/**
+ * gridControls
+ *
+ * creates an Hbox with various buttons,
+ * to specify the type of Grid and
+ * adds the Hbox to an existing BorderPane
+ *
+ * @author Brooke Keene
+ */
 public class gridControls {
+
 }
